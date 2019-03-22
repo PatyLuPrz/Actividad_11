0175a738c7a55dad750795f32e43a5a9df4c2e56
Paty Prz <1717110752@utectulancingo.edu.mx>
2019-03-22 17:54:48 -0600

    API clientes modificado

--- application/api/clientes/api_clientes.py
+++ application/api/clientes/api_clientes.py
@@ -28,7 +28,7 @@ class Api_clientes:
             web.header('Content-Type', 'application/json')
             return json.dumps(clientes_json)
 
-# http://0.0.0.0:8080/api_clientes?user_hash=12345&action=put&id_cliente=1&product=nuevo&description=nueva&stock=10&purchase_price=1&price_sale=3&product_image=0
+    # http://0.0.0.0:8080/api_clientes?user_hash=12345&action=put&nombre=Gabriela&apellido_paterno=Curiel&apellido_materno=Garcia&telefono=7751245625&email=gabi@gmail.com
     def put(self, nombre,apellido_paterno,apellido_materno,telefono,email):
         try:
             config.model.insert_clientes(nombre,apellido_paterno,apellido_materno,telefono,email)
@@ -39,7 +39,7 @@ class Api_clientes:
             print "PUT Error {}".format(e.args)
             return None
 
-# http://0.0.0.0:8080/api_clientes?user_hash=12345&action=delete&id_cliente=1
+# http://0.0.0.0:8080/api_clientes?user_hash=12345&action=delete&id_cliente=8
     def delete(self, id_cliente):
         try:
             config.model.delete_clientes(id_cliente)
@@ -51,6 +51,7 @@ class Api_clientes:
             return None
 
 # http://0.0.0.0:8080/api_clientes?user_hash=12345&action=update&id_cliente=1&product=nuevo&description=nueva&stock=10&purchase_price=1&price_sale=3&product_image=default.jpg
+# localhost:8080//api_clientes?user_hash=12345&action=update&id_cliente=9&nombre=Gabriela&apellido_paterno=Curiel&apellido_materno=Garcia&telefono=7751245625&email=gabi@gmail.com
     def update(self, id_cliente, nombre,apellido_paterno,apellido_materno,telefono,email):
         try:
             config.model.edit_clientes(id_cliente,nombre,apellido_paterno,apellido_materno,telefono,email)
@@ -77,12 +78,18 @@ class Api_clientes:
         try:
             user_hash = user_data.user_hash  # user validation
             action = user_data.action  # action GET, PUT, DELETE, UPDATE
-            id_cliente=user_data.id_cliente
-            nombre=user_data.nombre
-            apellido_paterno=user_data.apellido_paterno
-            apellido_materno=user_data.apellido_materno
-            telefono=user_data.telefono
-            email=user_data.email
+            id_cliente=user_data.id_cliente
+
+            nombre=user_data.nombre
+
+            apellido_paterno=user_data.apellido_paterno
+
+            apellido_materno=user_data.apellido_materno
+
+            telefono=user_data.telefono
+
+            email=user_data.email
+
             # user_hash
             if user_hash == '12345':
                 if action is None:
